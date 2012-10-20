bbf0e1e0968b085049c51ba34c62aeb17c4d80a3
JohnnyPP <johnnnyppp@gmail.com>
2012-10-20 23:45:37 +0200

    TMP102-v1

---
+++ .TMP102Arduino.vsarduino.h
@@ -0,0 +1,18 @@
+#define __AVR_ATmega328P__
+#define __cplusplus
+#define __builtin_va_list int
+#define __attribute__(x)
+#define __inline__
+#define __asm__(x)
+#define ARDUINO 100
+extern "C" void __cxa_pure_virtual() {}
+#include "C:\arduino-1.0\libraries\Wire\Wire.h"
+#include "C:\arduino-1.0\libraries\Wire\utility\twi.h"
+#include "C:\arduino-1.0\libraries\Wire\Wire.cpp"
+#include "C:\arduino-1.0\libraries\Wire\utility\twi.c"
+void setup();
+void loop();
+
+#include "C:\arduino-1.0\hardware\arduino\variants\standard\pins_arduino.h" 
+#include "C:\arduino-1.0\hardware\arduino\cores\arduino\Arduino.h"
+#include "C:\Users\Kolan\Documents\Arduino\TMP102Arduino\TMP102Arduino.pde" 
---
+++ TMP102Arduino.pde
@@ -0,0 +1,21 @@
+#include <Wire.h>
+
+int tmp102Address = 0x48;
+
+void setup()
+{
+	Serial.begin(9600);
+	Wire.begin();
+
+  /* add setup code here */
+
+	
+
+}
+
+void loop()
+{
+
+  /* add main program code here */
+
+}
